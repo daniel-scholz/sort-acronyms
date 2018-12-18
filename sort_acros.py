06059bbf53a2e1d9ca4fd341f3c1d7e673490b7d
Daniel Scholz <daniel.scholz@online.de>
2018-12-18 10:30:07 +0100

    Removes unnecessary print statements

--- sort_acros.py
+++ sort_acros.py
@@ -5,18 +5,17 @@ import os
 Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
 # show an "Open" dialog box and return the path to the selected file
 filename = askopenfilename()
-print(filename)
 with open(filename, "r+", encoding="utf-8") as f:
     lines = f.readlines()
-    print(lines)
     swap = True
+    print("sorting...")
     while swap:
         swap = False
         for i in range(len(lines)-1):
             if lines[i].replace("\t", "").startswith("\\acro") and lines[i+1].replace("\t", "").startswith("\\acro") and lines[i].lower() > lines[i+1].lower():
                 lines[i], lines[i+1] = lines[i+1], lines[i]
                 swap = True
-        print(lines)
     f.seek(0)
-
     f.writelines(lines)
+    print("done.")
+    
